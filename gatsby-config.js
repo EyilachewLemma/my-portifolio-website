2c037a827efd32f74686b943d1c709644a36a643
EyilachewLemma <eyilachewlema@gmail.com>
2023-01-27 08:11:42 +0300

    after removing post file

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Eyilachew Lemma',
     description:
       'Eyilachew Lemma is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://drjseifu.vercel.app/', // No trailing slash allowed!
+    siteUrl: 'https://eyilachew.vercel.app/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@eyilachew_lema',
   },
